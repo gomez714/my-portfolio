883c416187519357748d8cd8e2d5231e4f4d8c96
Luis Gomez <lgomez00714@gmail.com>
2025-03-16 22:31:53 -0700

    Fully implemented Header

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,18 +1,34 @@
 import { IconHexagonLetterL } from "@tabler/icons-react";
+import SideBar from "./SideBar";
+
+const links = ["About Me", "Experience", "Work", "Skills", "Contact"]
+
+const navLinks = (col: boolean) => {
+  return links.map((link, index) => {
+    return (
+      <a 
+        key={index} 
+        className={`${col ? 'flex flex-col items-center':''} text-textColor text-xl font-mono hover:text-primaryColor`} 
+        href={`#${link}`}
+      >
+        <span className="text-primaryColor">0{index+1}. </span>{link}
+      </a>
+    )
+  })
+}
 
 const Header = () => {
   return (
-    <nav className="flex justify-between h-[15vh] bg-bgColor font-mono px-10 items-center">
-      <IconHexagonLetterL className="text-primaryColor" size={60} stroke={1.25}/>
+    <nav className="flex justify-between h-[18vh] bg-bgColor px-10 items-center">
+      <IconHexagonLetterL className="z-10 text-primaryColor" size={60} stroke={1.25}/>
       <div className="bs:flex gap-8 hidden">
-        <a className="text-textColor hover:text-primaryColor" href="#about">About Me</a>
-        <a className="text-textColor hover:text-primaryColor" href="#experience">Experience</a>
-        <a className="text-textColor hover:text-primaryColor" href="#work">Work</a>
-        <a className="text-textColor hover:text-primaryColor" href="#contact">Contact</a>
-
+        {navLinks(false)}
       </div>
+      <SideBar />
     </nav>
+
   )
 }
 
 export default Header;
+export {navLinks};
